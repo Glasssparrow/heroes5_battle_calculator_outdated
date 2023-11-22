aa6920bb2ba0a1555ef255ab4f463af73069c0b6
Glass <125502088+Glasssparrow@users.noreply.github.com>
2023-11-22 22:49:14 +0300

    Update unit.py

--- unit.py
+++ unit.py
@@ -2,8 +2,23 @@
 
 class Unit:
 
-    def take_action(self):
+    def take_action(self, action_type, target):
+        self._start_turn()
+        self._action(action_type, target)
+        self._end_turn()
+
+    def _start_turn(self):
+        pass
+
+    def _action(self, action_type, target):
+        pass
+
+    def _end_turn(self):
         pass
 
     def __init__(self):
-        pass
+        self.actions = []
+        self.reactions = []
+        self.effects = []
+        self.skills = []
+        self.immunities = []
