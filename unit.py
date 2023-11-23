9ef155c1a5e25ce1b798c0241ae41efa4d44a9af
Glass <125502088+Glasssparrow@users.noreply.github.com>
2023-11-23 14:16:57 +0300

    Update unit.py

--- unit.py
+++ unit.py
@@ -16,9 +16,25 @@ class Unit:
     def _end_turn(self):
         pass
 
-    def __init__(self):
+    def __init__(self, attack, defence, min_damage, max_damage, health,
+                 initiative, speed):
         self.actions = []
         self.reactions = []
         self.effects = []
         self.skills = []
         self.immunities = []
+        self.atk = attack
+        self.defence = defence
+        self.min_damage_limit = min_damage
+        self.min_damage = min_damage
+        self.max_damage_limit = max_damage
+        self.max_damage = max_damage
+        self.health = health
+        self.initiative = initiative
+        self.speed = speed
+        self.quantity = 0
+        self.hp = 0
+
+    def get_quantity(self, quantity):
+        self.quantity = quantity
+        self.hp = quantity * self.health
