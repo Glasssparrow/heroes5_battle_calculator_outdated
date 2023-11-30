5cb3bad7c00bb4664a998bda94e46b0333789da6
Glass <125502088+Glasssparrow@users.noreply.github.com>
2023-11-30 21:15:24 +0300

    calculate_damage moved to different file

--- unit/actions/melee.py
+++ unit/actions/melee.py
@@ -1,19 +1,6 @@
 from keywords import *
-from math import copysign, floor
 from random import randint
-
-
-def calculate_damage(damage, attack, defence, max_damage):
-    sign = copysign(1, attack - defence)
-    amount_of_damage = (
-        damage * (1 + 0.05 * abs(attack - defence)) ** sign
-    )
-    # По игровой механике урон округляется вниз
-    amount_of_damage = floor(amount_of_damage)
-    if amount_of_damage > max_damage:
-        return max_damage
-    else:
-        return amount_of_damage
+from common import calculate_damage
 
 
 class Melee:
