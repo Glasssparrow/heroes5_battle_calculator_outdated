e1511a4e5d4313db5b296a603ce2b40f28039f86
Glass <125502088+Glasssparrow@users.noreply.github.com>
2023-12-02 19:29:38 +0300

    Update keywords.py

--- keywords.py
+++ keywords.py
@@ -3,3 +3,6 @@
 MELEE_ATTACK = "melee_attack"
 MELEE_COUNTER = "melee_counter"
 ACTIVATE_BEFORE_STRIKE = "before strike"
+ACTIVATE_AFTER_STRIKE = "after strike"
+ACTIVATE_BEFORE_SHOOT = "before shot"
+ACTIVATE_AFTER_SHOOT = "after shot"
