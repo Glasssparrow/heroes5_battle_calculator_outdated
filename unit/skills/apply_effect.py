7944e0a141713afc02f3868ea0ebc310823dce10
Glass <125502088+Glasssparrow@users.noreply.github.com>
2023-12-02 15:42:37 +0300

    skills placeholder

---
+++ unit/skills/apply_effect.py
@@ -0,0 +1,7 @@
+from .common import Skill
+
+
+class ApplyEffect(Skill):
+
+    def __init__(self, owner):
+        super().__init__(owner)
---
+++ unit/skills/common.py
@@ -0,0 +1,7 @@
+
+
+class Skill:
+
+    def __init__(self, owner):
+        self.owner = owner
+        self.name = "default"
