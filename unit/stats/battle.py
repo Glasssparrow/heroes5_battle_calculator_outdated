af4974e96fc4bfe629fb54e064cf679ae9b66851
Glass <125502088+Glasssparrow@users.noreply.github.com>
2023-12-03 19:37:29 +0300

    damage and initiative

--- unit/stats/battle.py
+++ unit/stats/battle.py
@@ -1,14 +1,35 @@
 from .common import Stat
+from math import floor
 
 
 class MinDamage(Stat):
 
-    pass
+    def __get__(self, instance, owner):
+        result = instance.__dict__[self.name]
+        for buff in instance.__dict__["effects"]:
+            result += buff.__dict__[self.stat]
+        result = floor(result)
+        if result < 0:
+            return 0
+        elif result < instance.__dict__["_min_damage_limit"]:
+            return instance.__dict__["_min_damage_limit"]
+        else:
+            return result
 
 
 class MaxDamage(Stat):
 
-    pass
+    def __get__(self, instance, owner):
+        result = instance.__dict__[self.name]
+        for buff in instance.__dict__["effects"]:
+            result += buff.__dict__[self.stat]
+        result = floor(result)
+        if result < 0:
+            return 0
+        elif result > instance.__dict__["_max_damage_limit"]:
+            return instance.__dict__["_max_damage_limit"]
+        else:
+            return result
 
 
 class Health(Stat):
--- unit/stats/initiative_and_movement.py
+++ unit/stats/initiative_and_movement.py
@@ -3,7 +3,15 @@ from .common import Stat
 
 class Initiative(Stat):
 
-    pass
+    def __get__(self, instance, owner):
+        result = instance.__dict__[self.name]
+        for buff in instance.__dict__["effects"]:
+            result += (buff.__dict__[self.stat] *
+                       instance.__dict__[self.name])
+        if result < 0.1:
+            return 0.1
+        else:
+            return result
 
 
 class Speed(Stat):
