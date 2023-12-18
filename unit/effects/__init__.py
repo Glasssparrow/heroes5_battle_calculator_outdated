fa9b0d160c95625a7f5d23ef2e584da33d4fe676
Glass <125502088+Glasssparrow@users.noreply.github.com>
2023-12-18 20:36:37 +0300

    blind

--- unit/effects/__init__.py
+++ unit/effects/__init__.py
@@ -6,5 +6,5 @@ from .initiative import *
 
 __all__ = [
     "Block1Counterattack", "DexterityBuff", "LowMoraleEffect",
-    "HighMoraleEffect", "BattleFrenzy", "Bash"
+    "HighMoraleEffect", "BattleFrenzy", "Bash", "Blind"
 ]
--- unit/effects/debuffs.py
+++ unit/effects/debuffs.py
@@ -9,3 +9,12 @@ class Block1Counterattack(Effect):
         self.name = "block_1_counter"
         self.special_effects.append(TEMPORARY_BLOCK_COUNTER)
         self.dispell_conditions.append(DISPELL_TEMPORARY_BLOCK_COUNTER)
+
+
+class Blind(Effect):
+
+    def __init__(self):
+        super().__init__()
+        self.name = "ослепление"
+        self.special_effects.append(BLOCK_ACTION)
+        self.dispell_conditions.append(DISPELL_AFTER_TAKING_DAMAGE)
