ddf2908f6d1bf0ea59ef5a246a432cf873744ee3
Glass <125502088+Glasssparrow@users.noreply.github.com>
2023-12-19 22:17:34 +0300

    comments in keywords.py

--- keywords.py
+++ keywords.py
@@ -1,22 +1,34 @@
 
 
+# Типы действий
 MELEE_ATTACK = "melee_attack"
+
+# Типы реакций
 MELEE_COUNTER = "melee_counter"
+
+# Условия активации навыков
 ACTIVATE_BEFORE_STRIKE = "before strike"
 ACTIVATE_AFTER_STRIKE = "after strike"
 ACTIVATE_BEFORE_SHOOT = "before shot"
 ACTIVATE_AFTER_SHOOT = "after shot"
 ACTIVATE_AT_TURN_START = "turn_start"
 ACTIVATE_AT_TURN_END = "turn_end"
+
+# Метка для инициативы
 INITIATIVE_MORALE = "morale_mark_for_initiative"
+
+# Условия снятия эффектов
 DISPELL_CASE_INITIATIVE = "dispel_in_initiative_package"
 DISPELL_AT_TURN_START = "dispelled_at_start_of_the_turn"
 DISPELL_AT_TURN_END = "dispelled_at_end_of_the_turn"
 DISPELL_TEMPORARY_BLOCK_COUNTER = "dispelled_at_failed_counter"
 DISPELL_AFTER_TAKING_DAMAGE = "dispelled_after_taking_damage"
+
+# Особенности эффектов
 BLOCK_ACTION = "block_action"
 BLOCK_COUNTER = "block_counterattack"
 TEMPORARY_BLOCK_COUNTER = "block_1_counterattack"
 BASH = "bash"
 
+# Для словаря modifiers в классе Effect
 BATTLE_FRENZY_MODIFIER = "battle_frenzy_modifier"
