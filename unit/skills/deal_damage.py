04881a437696b4d53b72192c778ebebcc87ca296
Glass <125502088+Glasssparrow@users.noreply.github.com>
2023-12-21 21:01:45 +0300

    Update deal_damage.py

--- unit/skills/deal_damage.py
+++ unit/skills/deal_damage.py
@@ -1,4 +1,8 @@
+from .common import Skill, calculate_base_chance
+from ..common import check_random
+from unit.effects import *
+from keywords import *
 
 
-class Kill1Extra:
+class Kill1Extra(Skill):
     pass
