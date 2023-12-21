96e2a448e37549f793d7301404dbf116faa188e0
Glass <125502088+Glasssparrow@users.noreply.github.com>
2023-12-21 20:56:36 +0300

    Kill1Extra placeholder

--- unit/skills/__init__.py
+++ unit/skills/__init__.py
@@ -1,8 +1,9 @@
 from .apply_effect import *
 from .healing import *
 from .turnend import *
+from .deal_damage import *
 
 __all__ = [
     "PeasantBash", "Vampire", "HighMorale", "LowMorale",
-    "Dexterity", "BlindingStrike"
+    "Dexterity", "BlindingStrike", "Kill1Extra"
 ]
---
+++ unit/skills/deal_damage.py
@@ -0,0 +1,4 @@
+
+
+class Kill1Extra:
+    pass
