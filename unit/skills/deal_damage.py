85ec561f006fd214ea6482d2b0d309e465155d38
Glass <125502088+Glasssparrow@users.noreply.github.com>
2023-12-21 21:10:14 +0300

    Kill1Extra

--- unit/skills/deal_damage.py
+++ unit/skills/deal_damage.py
@@ -1,8 +1,16 @@
-from .common import Skill, calculate_base_chance
-from ..common import check_random
-from unit.effects import *
+from .common import Skill
 from keywords import *
 
 
 class Kill1Extra(Skill):
-    pass
+
+    def __init__(self, owner):
+        super().__init__(owner)
+        self.name = "Убивает на 1 существо больше"
+        self.activation_cases.append(ACTIVATE_AFTER_STRIKE)
+
+    @staticmethod
+    def use(target, damage, kills, battle_map):
+        killed_by_skill = target.take_damage(target.health)
+        print(f"{target.name} получает {damage} урона. Погибло "
+              f"{killed_by_skill} существ.")
