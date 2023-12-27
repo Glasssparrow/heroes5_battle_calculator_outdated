acc3ae37d316f1b7c6d4243e0f23fd24efc2e7fa
Glass <125502088+Glasssparrow@users.noreply.github.com>
2023-12-27 20:52:17 +0300

    Update unit.py

--- unit/unit.py
+++ unit/unit.py
@@ -104,7 +104,7 @@ class Unit:
         self.auras = []
         self.effects = []
         self.immunities = []
-        self.special_attributes = []   # Призрак, Невидимость
+        self.special_attributes = []   # Призрак
         self.attack = attack
         self.defence = defence
         self.min_damage = min_damage
