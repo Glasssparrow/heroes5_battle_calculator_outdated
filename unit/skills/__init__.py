18e8e577e58f78d9844a3bd478ca4c0bc6f26689
Glass <125502088+Glasssparrow@users.noreply.github.com>
2023-12-30 16:22:39 +0300

    file for pushing skills

--- unit/skills/__init__.py
+++ unit/skills/__init__.py
@@ -2,6 +2,7 @@ from .apply_effect import *
 from .healing import *
 from .turnend import *
 from .deal_damage import *
+from .push import *
 
 __all__ = [
     "PeasantBash", "Vampire", "HighMorale", "LowMorale",
