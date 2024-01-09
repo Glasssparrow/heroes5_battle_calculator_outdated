ca98b8191a510ce3696ffebf1ce5f233d56b49ec
Glass <125502088+Glasssparrow@users.noreply.github.com>
2024-01-09 19:28:53 +0300

    for small units

--- map_and_movement/get_distance.py
+++ map_and_movement/get_distance.py
@@ -1,4 +1,10 @@
 
 
 def get_distance(battle_map, unit1, unit2):
-    pass
+    if not unit1.big and not unit2.big:
+        x_distance = abs(unit1.coord[0] - unit2.coord[0])
+        y_distance = abs(unit1.coord[1] - unit2.coord[1])
+        if x_distance == y_distance == 1:
+            return 1
+        else:
+            return x_distance+y_distance
