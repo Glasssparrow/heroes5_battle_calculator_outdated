ed0b3bac3f41cf108aeb0aa6896a3fae0bffedf9
Glass <125502088+Glasssparrow@users.noreply.github.com>
2024-01-14 14:46:35 +0300

    Update get_available_cells.py

--- map_and_movement/get_available_cells.py
+++ map_and_movement/get_available_cells.py
@@ -1,4 +1,6 @@
+from .dijkstra_on_grid import Pathfinder
 
 
 def get_available_cells(battle_map, unit):
-    pass
+    pathfinder = Pathfinder(10, 12)
+    return pathfinder(unit.coord[0], unit.coord[1], unit.speed)
