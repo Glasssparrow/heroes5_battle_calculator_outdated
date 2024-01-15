ccfb33fdd2bec5ed6f7bec34609c3ba52c716bf4
Glass <125502088+Glasssparrow@users.noreply.github.com>
2024-01-15 22:21:29 +0300

    Update battle_map.py

--- map_and_movement/battle_map.py
+++ map_and_movement/battle_map.py
@@ -35,6 +35,9 @@ class BattleMap:
         unit.id = len(self.units)
         self.units.append(unit)
 
+    def _add_unit_on_grid(self):
+        pass
+
     @staticmethod
     def get_distance(unit1, unit2):
         return get_distance(unit1, unit2)
