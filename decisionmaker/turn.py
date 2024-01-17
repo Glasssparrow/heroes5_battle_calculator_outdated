f2fde9c57d3232841ff858ad7213c5c1a6909be6
Glass <125502088+Glasssparrow@users.noreply.github.com>
2024-01-17 21:30:39 +0300

    Update turn.py

--- decisionmaker/turn.py
+++ decisionmaker/turn.py
@@ -3,5 +3,4 @@
 def act(unit, battle_map):
     available_cells = battle_map.get_available_cells(unit)
     for coord, length, path in available_cells:
-        print(coord, length, path)
         pass
