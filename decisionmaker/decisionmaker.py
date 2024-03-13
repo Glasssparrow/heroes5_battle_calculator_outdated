c80213209315df9515cd746cc38ffa26dc95595d
Glass <125502088+Glasssparrow@users.noreply.github.com>
2024-03-13 21:55:58 +0300

    decisionmaker placeholder

--- decisionmaker/decisionmaker.py
+++ decisionmaker/decisionmaker.py
@@ -2,4 +2,32 @@
 
 class DecisionMaker:
 
-    pass
+    def __init__(self):
+        self.coords = []
+        self.height = {}
+
+    def __getitem__(self, item):
+        for n, coord in enumerate(self.coords):
+            if coord == item:
+                return self.height[n]
+        raise Exception(f"{item} вне диапазона.")
+
+    def __setitem__(self, key, value):
+        number = None
+        for n, coord in enumerate(self.coords):
+            if coord == key:
+                number = n
+        if number is None:
+            raise Exception(f"{key} вне диапазона")
+        else:
+            self.height[number] += value
+
+    def __delitem__(self, key):
+        number = None
+        for n, coord in enumerate(self.coords):
+            if coord == key:
+                number = n
+        if number is None:
+            raise Exception(f"{key} вне диапазона")
+        else:
+            self.height[number] = 0
