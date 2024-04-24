efc8c46e343e4431d2ff1a47be81d3c639bc1018
Glass <125502088+Glasssparrow@users.noreply.github.com>
2024-04-24 16:42:59 +0300

    Update reach_map.py

--- map_and_movement/reach_map.py
+++ map_and_movement/reach_map.py
@@ -3,8 +3,8 @@
 class ReachMap:
 
     def __init__(self, unit, length, height):
-        self.threat_map_for_big_enemies = []
-        self.threat_map_for_small_enemies = []
+        self.threat_for_big = []
+        self.threat_for_small = []
         self.map_length = length
         self.map_height = height
         self.enemy_big = False
@@ -13,6 +13,6 @@ class ReachMap:
         # Возвращает величину угрозы для выбранной клетки
         x, y = coord[0], coord[1]
         if self.enemy_big:
-            return self.threat_map_for_big_enemies[x+y*self.map_length]
+            return self.threat_for_big[x + y * self.map_length]
         else:
-            return self.threat_map_for_small_enemies[x + y * self.map_length]
+            return self.threat_for_small[x + y * self.map_length]
