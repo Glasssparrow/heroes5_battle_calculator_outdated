02c64dc3290c5e54b100888874270aceb01e1ba4
Glass <125502088+Glasssparrow@users.noreply.github.com>
2024-04-24 16:31:23 +0300

    Update reach_map.py

--- map_and_movement/reach_map.py
+++ map_and_movement/reach_map.py
@@ -3,5 +3,6 @@
 class ReachMap:
 
     def __init__(self, unit):
-        self.cells = []
+        self.threat_map_for_big_enemies = []
+        self.threat_map_for_small_enemies = []
         self.enemy_big = False
