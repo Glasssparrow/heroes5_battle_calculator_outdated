0a67a0aef939ef330e3897fc66739a91953e929d
Glass <125502088+Glasssparrow@users.noreply.github.com>
2024-04-26 17:57:35 +0300

    Update reach_map.py

--- map_and_movement/reach_map.py
+++ map_and_movement/reach_map.py
@@ -8,6 +8,17 @@ class ReachMap:
         self.map_length = length
         self.map_height = height
         self.enemy_big = False
+        melee_threat = 0
+        for action in unit.actions:
+            if action.range == 1 and action.threat > melee_threat:
+                melee_threat = action.threat
+        threat_dict = {}
+        for action in unit.actions:
+            if (
+                action.range not in threat_dict.keys() or
+                threat_dict[action.range] < action.threat
+            ):
+                threat_dict[action.range] = action.threat
         for x in range(height):
             for y in range(length):
                 self.threat_for_big.append(0)
