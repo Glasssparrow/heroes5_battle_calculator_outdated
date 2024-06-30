daa08360e85eb682052f46e1087183f9d0270d81
Glass <125502088+Glasssparrow@users.noreply.github.com>
2024-06-30 22:56:10 +0300

    Update get_available_cells.py

--- map_and_movement/get_available_cells.py
+++ map_and_movement/get_available_cells.py
@@ -18,5 +18,5 @@ def get_available_cells(pathfinder_big, pathfinder_small, unit):
 def get_danger_zone(battle_map, unit):
     danger_zone = Path(unit.coord[0], unit.coord[1],
                        battle_map._map_length)
-    for side_name, units_list in battle_map.sides.items():
+    for side_name, unit_ids_list in battle_map.sides.items():
         pass
