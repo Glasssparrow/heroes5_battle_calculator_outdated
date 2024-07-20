4e5cc3e9c8a0c1a81569be85123e1a1b697c4c2e
Glass <125502088+Glasssparrow@users.noreply.github.com>
2024-07-20 21:48:32 +0300

    Update get_available_cells.py

--- map_and_movement/get_available_cells.py
+++ map_and_movement/get_available_cells.py
@@ -105,9 +105,14 @@ def get_available_cells(pathfinder_big, pathfinder_small, unit):
 
 
 def get_danger_zone(battle_map, unit):
-    danger_zone = Path(unit.coord[0], unit.coord[1],
-                       battle_map.map_length)
+    result = DangerZone(
+        height=battle_map.map_height,
+        length=battle_map.map_length,
+    )
+    path = Path(unit.coord[0], unit.coord[1],
+                battle_map.map_length)
     for side_name, unit_ids_list in battle_map.sides.items():
         pass
+    return result
 
     # Возвращаем экземпляр класса DangerZone
